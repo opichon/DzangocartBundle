096d113d63a0b6be14d46e20be7f73441b77c06e
Alexander Kozlov <alexskozlov@gmail.com>
2012-11-13 13:03:54 +0400

    added add_to_cart helper function

--- Twig/Extension/DzangocartExtension.php
+++ Twig/Extension/DzangocartExtension.php
@@ -1,32 +1,81 @@
 <?php
-namespace Dzangocart\DangocartBundle\Twig\Extension;
+namespace Dzangocart\Bundle\DzangocartBundle\Twig\Extension;
 
-use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Dzangocart\Bundle\DzangocartBundle\Service\Dzangocart;
 
 class DzangocartExtension extends \Twig_Extension
 {
-
-	protected $loader;
-	protected $controller;
-
-	public function __construct(FilesystemLoader $loader)
+	protected $container;
+	protected $dzangocart;
+	
+	public function __construct(ContainerInterface $container, Dzangocart $dzangocart)
 	{
-		$this->loader = $loader;
+		$this->container = $container;
+		$this->dzangocart = $dzangocart;
 	}
 
-	public function setController($controller)
-	{
-		$this->controller = $controller;
-	}
-
+	public function getName()
+    {
+        return 'dzangocart';
+    }
+    
 	/**
 	 * {@inheritdoc}
 	 */
 	public function getFunctions()
 	{
 		return array(
-			'code' => new \Twig_Function_Method($this, 'add_to_cart', array('is_safe' => array('html'))),
+			'add_to_cart' => new \Twig_Function_Method($this, 'add_to_cart', array('is_safe' => array('html'))),
 		);
 	}
+    
+    public function add_to_cart($product, $price, $quantity = 1,
+                                $category = 'default', $options = array(),
+                                $checkout = false, $label = null, $html_options = array())
+    {
+        if (!$label) {
+            $label = 'Add to cart';
+        }
+
+        $html_options = array_merge(array('title' => 'Shopping cart'), $html_options);
+        $html_options['class'] = array_key_exists('class', $html_options) ?
+                                    'dzangocart ' . $html_options['class'] :
+                                    'dzangocart';        
+        
+        
+        $html_options['href'] = $this->getCartUrl($product, $price, $quantity, 
+                                                  $category, $options, $checkout);
+        
+        $attributes = "";
+        foreach ($html_options as $key => $value) {
+            $attributes .= ' ' . $key . '="' . htmlspecialchars($value) . '"';
+        }
+        
+        return '<a' . $attributes . '>' . $label . '</a>';
+    }
+    
+    protected function getCartUrl($product, $price, $quantity = 1,
+                                  $category = 'default', $options = array(),
+                                  $checkout = false)
+    {
+        $url = $this->dzangocart->getUrl();
+        $url .= '/cart/show?';
+        
+        $params = array('name' => is_object($product) ? $product->__toString() : $product,
+                        'price' => $price);
+        
+        if ($quantity) { $params['quantity'] = $quantity; }
+        if ($category) { $params['category'] = $category; }        
+        if ($checkout) { $params['checkout'] = true; }
+
+        if (!array_key_exists('test', $options) && $test = $this->dzangocart->getTestCode()) {
+            $options['test'] = $test;
+        }
+        
+        $params = array_merge($params, $options);
+        
+        return $url . http_build_query($params);
+    }
 }
 
