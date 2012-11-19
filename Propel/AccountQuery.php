013fb1b2a5967dfeaaddef2c22c2813bef26efe4
Olivier Pichon <op@united-asian.com>
2012-11-19 09:36:20 +0700

    cleaned up code

--- Propel/AccountQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dzangocart\Bundle\DzangocartBundle\Propel;
-
-use Dzangocart\Bundle\DzangocartBundle\Propel\om\BaseAccountQuery;
-
-class AccountQuery extends BaseAccountQuery
-{
-}
