07829d9d770314d22034b1cc1f2023377f9fd5b5
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-06-13 15:47:47 +0545

    initialized catalogue layout

---
+++ Resources/public/js/scripts/catalogue.js
@@ -0,0 +1,41 @@
+!function( $ ) {
+	$.fn.catalogue = function( method ) {
+
+		var settings,
+			table;
+
+		// Public methods
+		var methods = {
+			init: function( options ) {
+				settings = $.extend( true, {}, $.fn.catalogue.defaults, options );
+
+				return this.each(function() {
+					var $this = $( this );
+
+					$( "table.treetable", this )
+						.treetable( settings.treetable );
+				});
+			}
+		};
+
+		if ( methods[ method ] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+		}
+		else if ( typeof method === "object" || !method ) {
+			return methods.init.apply( this, arguments );
+		}
+		else {
+			$.error( "Method " +  method + " does not exist in $.catalogue." );
+		}
+	};
+
+	$.fn.catalogue.defaults = {
+		treetable: {
+			expandable: true
+		}
+	};
+} ( window.jQuery );
+
+$( document ).ready(function() {
+	$( '.catalogue' ).catalogue( );
+});
