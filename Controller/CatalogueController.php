999c9d18e9ff818f148700d070ce71ddc8616607
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-06-18 18:10:40 +0545

    updated controller and view file

--- Controller/CatalogueController.php
+++ Controller/CatalogueController.php
@@ -9,10 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class CatalogueController extends Controller
-{
-    protected static $count = 0;
-    protected static $items = array();
-    
+{   
     /**
      * @Route("/", name="dzangocart_catalogue")
      * @Template()
@@ -22,32 +19,8 @@ class CatalogueController extends Controller
         $catalogue = $this->get('dzangocart')
             ->getCatalogue();
 
-        $this->getTree($catalogue);
-
         return array(
-            'catalogue' => self::$items
+            'catalogue' => $catalogue
         );
-    }
-
-    protected function getTree($catalogue, $parent_id = 0 )
-    {
-        foreach ($catalogue as $item) {
-            self::$count += 1;
-
-            $item['parent_id'] = $parent_id;
-
-            self::$items[self::$count] = array(
-                'parent_id' => $item['parent_id'],
-                'name' => $item['name'],
-                'code' => $item['code'],
-                'pcode' => $item['pcode'],
-                'price' => $item['price'],
-                'tax_included' => $item['taxIncluded']
-            );
-
-            if (array_key_exists('categories', $item)) {
-                self::getTree($item['categories'], self::$count);
-            }
-        }
     }
 }
