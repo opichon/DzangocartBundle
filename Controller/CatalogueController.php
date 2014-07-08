cf9656abaa0433a421a1764d9f410cb932d0c6bc
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-07-08 14:17:15 +0545

    forwarded data from showAction to updateAction

--- Controller/CatalogueController.php
+++ Controller/CatalogueController.php
@@ -8,16 +8,23 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Component\Form\FormFactory;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class CatalogueController
 {
     protected $dzangocart;
     protected $form_factory;
+    protected $request_stack;
+    protected $http_kernel;
 
-    public function __construct($dzangocart, FormFactory $form_factory)
+    public function __construct($dzangocart, FormFactory $form_factory, RequestStack $request_stack, HttpKernel  $http_kernel)
     {
         $this->dzangocart = $dzangocart;
         $this->form_factory = $form_factory;
+        $this->request_stack = $request_stack;
+        $this->http_kernel = $http_kernel;
     }
 
     /**
@@ -53,7 +60,11 @@ class CatalogueController
         $form->handleRequest($request);
 
         if( $form ->isValid()){
-            
+            $path = array('category' => $category);
+            $path['_controller'] = 'dzangocart.controller.catalogue:updateAction';
+            $subRequest = $this->request_stack->getCurrentRequest()->duplicate(array(), null, $path);
+
+            return $this->http_kernel->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
         }
 
         return array(
