0a500202ad36e49380999e26b972aeee32ed1ace
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-08-19 14:09:45 +0545

    added flashbag with translations

--- Controller/CatalogueController.php
+++ Controller/CatalogueController.php
@@ -4,12 +4,16 @@ namespace Dzangocart\Bundle\DzangocartBundle\Controller;
 
 use Dzangocart\Bundle\DzangocartBundle\Form\Type\CategoryFormType;
 
+use Exception;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Router;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class CatalogueController
 {
@@ -17,13 +21,15 @@ class CatalogueController
     protected $form_factory;
     protected $router;
     protected $session;
+    protected $translator;
 
-    public function __construct($dzangocart, FormFactory $form_factory, Router $router, Session $session)
+    public function __construct($dzangocart, FormFactory $form_factory, Router $router, Session $session, TranslatorInterface $translator)
     {
         $this->dzangocart = $dzangocart;
         $this->form_factory = $form_factory;
         $this->router = $router;
         $this->session = $session;
+        $this->translator = $translator;
     }
 
     /**
@@ -48,8 +54,17 @@ class CatalogueController
             'id' => $id
         );
 
-        $category = $this->dzangocart
-            ->getCategory($params);
+        try {
+            $category = $this->dzangocart
+                ->getCategory($params);
+        } catch (Exception $e){
+            $this->session->getFlashBag()->add(
+                'Category.error',
+                $this->translator->trans('category.error.not_found', array(), 'dzangocart', $request->getLocale())
+            );
+
+            return new RedirectResponse($this->router->generate('dzangocart_catalogue'));
+        }
 
         $form = $this->form_factory->create(
             new CategoryFormType(),
