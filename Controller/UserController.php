f5d8b56e8c61eac8e95a9962ad21f7e275428fc6
milan <milan@crossovernepal.com>
2014-09-15 11:38:42 +0545

    created /purchases route and stup action in UserController

---
+++ Controller/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dzangocart\Bundle\DzangocartBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function purchasesAction(Request $request)
+    {
+        $params = array(
+            'limit' => $request->query->get('length'),
+            'offset' => $request->query->get('start')
+        );
+
+        $data = $this->get('dzangocart')
+            ->getSales($params);
+
+        return array(
+            'data' => $data
+        );
+    }
+}
